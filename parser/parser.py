a6c827bdc841c90d8a92033238f4bdc73d6d9093
tyoma crayfos <artemgusevw@mail.ru>
2024-05-26 23:20:26 +0300

    Добавить парсер youdo

--- parser/parser.py
+++ parser/parser.py
@@ -1,11 +1,13 @@
 from web.parser.habr.habr import start_habr_parser
 from web.parser.fl_ru.parser import start_fl_parser
 from web.parser.kwork.kwork_parser import start_kwork_parser
+from web.parser.youdo.youdo_parser import start_youdo_parser
 
 def start_parser():
     start_habr_parser()
     start_fl_parser()
     start_kwork_parser()
+    start_youdo_parser()
 
 
 if __name__ == "__main__":
